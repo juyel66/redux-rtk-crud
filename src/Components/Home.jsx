fd4095c6f86abac88aede964c31a9cd20cd72b66
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-07-26 11:20:52 +0600

    use redux delete data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deletePost, fetchPost } from "../features/postSlice";
+import { Link } from "react-router";
 
 const Home = () => {
   const { isLoading, posts, error } = useSelector((state) => state.posts);
@@ -14,17 +15,23 @@ const Home = () => {
 
   console.log(posts);
 
-   const handleDelete = (id) => {
+const handleDelete = (id) => {
+  console.log("Deleting ID:", id);  // ✅ check
+  if (window.confirm("Are you sure you want to delete this user?")) {
     dispatch(deletePost(id));
-  };
+  }
+};
 
-
+ 
 
 
 
   return (
 <div className="container mx-auto">
   <h3 className="text-center mt-5 text-3xl font-semibold mb-3">Total Data is: {posts.length}</h3>
+ <div className="flex items-center justify-end">
+     <Link target="_blank" to="https://southest-asia.netlify.app/AddTourists" className="text-center mt-5 text-white px-2 py-1 rounded-[5px] bg-green-500 mb-3">Add Data</Link>
+ </div>
 
   {isLoading && <h1>Loading .....</h1>}
   {error && <h1>{error}</h1>}
@@ -32,12 +39,12 @@ const Home = () => {
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
     {posts && posts.map((post, index) => (
       <div key={post._id} className="border-2 border-green-600 p-4 rounded shadow">
-        <img className="w-full h-48 object-cover mb-2" src={post.photoURL} alt={post.name} />
-        <p className="text-green-500 font-semibold text-center">{index + 1}. {post.name}</p>
+        <img className="w-full h-48 object-cover mb-2" src={post.image} alt={post.name} />
+        <p className="text-green-500 font-semibold text-center">{index + 1}. {post.location}</p>
        <div className="flex items-center justify-center">
           <button
-                onClick={() => handleDelete(post._id)}
-                className="bg-red-600 text-white px-3 py-1 rounded mt-2 "
+                 onClick={() => handleDelete(post._id)}
+                className="text-gray-500 font-semibold border-green-400 hover:bg-red-600 cursor-pointer hover:text-white hover:border-white border-2  px-3 py-1 rounded mt-2  "
               >
                 Delete
               </button>
--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -1,48 +1,59 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
-export const fetchPost = createAsyncThunk("posts/fetchPost", async()=>{
-    const res = await axios.get("https://server-theta-umber.vercel.app/user");
-    return res.data
-
-})
-const postSlice = createSlice({
-    name: 'posts',
-    initialState: {
-        isLoading: false,
-        posts: [],
-        error: null
-    },
-
-     reducers: {
-    deletePost: (state, action) => {
-      // action.payload হিসেবে id বা unique key আশা করছি
-      state.posts = state.posts.filter((post) => post._id !== action.payload);
-    },
-  },
-
-
-
-    extraReducers: (builder) => {
-        builder.addCase(fetchPost.pending, (state) => {
-            state.isLoading = true;
-        })
-        builder.addCase(fetchPost.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.posts = action.payload;
-            state.error = null
-        })
-        builder.addCase(fetchPost.rejected, (state, action) => {
-            state.isLoading = false;
-            state.posts = [];
-            state.error = action.error.message;
-        });
-
-    }
-
-  
-
+export const fetchPost = createAsyncThunk("posts/fetchPost", async () => {
+  const res = await axios.get("https://southeast-asia-server-three.vercel.app/addTourist");
+  return res.data;
 });
-export const { deletePost } = postSlice.actions;
+
+// delete the server
+export const deletePost = createAsyncThunk("posts/deletePost", async (id) => {
+  await axios.delete(`https://southeast-asia-server-three.vercel.app/addTourist/${id}`);
+  return id;
+});
+
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    isLoading: false,
+    posts: [],
+    error: null,
+  },
+  reducers: {},
+
+  // delete only ui
+
+  //      reducers: {
+  //     deletePost: (state, action) => {
+  //       // action.payload হিসেবে id বা unique key আশা করছি
+  //       state.posts = state.posts.filter((post) => post._id !== action.payload);
+  //     },
+  //   },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchPost.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchPost.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.posts = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchPost.rejected, (state, action) => {
+      state.isLoading = false;
+      state.posts = [];
+      state.error = action.error.message;
+    });
+
+    // delete the server
+    builder.addCase(deletePost.fulfilled, (state, action) => {
+      state.posts = state.posts.filter((post) => post._id !== action.payload);
+    });
+  },
+});
+
+// delete only ui
+// export const { deletePost } = postSlice.actions;
+
 export default postSlice.reducer;
